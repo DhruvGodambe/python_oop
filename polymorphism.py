ef17a556faeed1d5e32b8c070d517d0c94782b86
DhruvGodambe <dhruvrockstone@gmail.com>
2020-09-20 18:11:08 +0530

    completed polymorphism

--- polymorphism.py
+++ polymorphism.py
@@ -1,3 +1,20 @@
-class pet:
-    def __init__(self):
-        pass
+class Pet:
+    def __init__(self, breed, color):
+        self.breed = breed
+        self.color = color
+
+    def show_data(self):
+        return "Hi, I am a " + self.breed + ", I am " + self.color + " in color"
+
+class Dog(Pet):
+    def __init__(self, breed, color, age):
+        super().__init__(breed, color)
+        self.age = age
+
+    #method overriding
+    def show_data(self):
+        return "Hi, I am a " + self.breed + ", I am " + self.color + " in color and my age is " + str(self.age)
+
+
+d1 = Dog("mutt", "brown", 10)
+print(d1.show_data())
